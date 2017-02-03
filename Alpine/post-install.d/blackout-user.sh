ea1148f27a5d8d45a6cf7eafec3563ef4d6d9b97
Michael Squires <blackout@repmovsb.com>
2017-02-03 23:34:15 +0000

    Update blackout-user.sh

--- Alpine/post-install.d/blackout-user.sh
+++ Alpine/post-install.d/blackout-user.sh
@@ -9,5 +9,5 @@ usermod -aG docker ${USERNAME}
 usermod -aG wheel ${USERNAME}
 usermod -s /bin/bash ${USERNAME}
 
-su -c "git clone https://gitlab.com/__blackout/config.git ~/.config" blackout
-su -c "~/.config/install.sh" blackout
+su - -c "git clone https://gitlab.com/__blackout/config.git ~/.config" blackout
+su - -c "~/.config/install.sh" blackout
