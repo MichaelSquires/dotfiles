e9f7c9bf32e3feb3094549dac90d68c8f9e094ae
Michael Squires <blackout@repmovsb.com>
2020-05-29 10:16:46 -0400

    Fish customization

---
+++ fish/.config/fish/functions/fish_vcs_prompt.fish
@@ -0,0 +1,9 @@
+function fish_vcs_prompt --description 'Print the prompts for all available vcsen'
+    if ! set -q TMUX
+        # If a prompt succeeded, we assume that it's printed the correct info.
+        # This is so we don't try svn if git already worked.
+        fish_git_prompt
+        or fish_hg_prompt
+        or fish_svn_prompt
+    end
+end
