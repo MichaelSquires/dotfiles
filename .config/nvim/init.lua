95a0d75d29f737974c32b3e2184974f7800dcb56
blackout <blackout@vertex.link>
2024-07-28 16:37:28 -0400

    diagnostics config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -5,3 +5,23 @@ vim.cmd([[
     let &packpath = &runtimepath
     source ~/.vimrc
 ]])
+
+-- Diagnostics configuration
+vim.diagnostic.config({
+  virtual_text = {
+    source = "if_many",
+    prefix = '●', -- Could be '■', '▎', 'x'
+  },
+  severity_sort = true,
+  float = {
+    source = true,  -- Or "if_many"
+  },
+})
+
+vim.lsp.handlers["textDocument/publishDiagnostics"] =
+    vim.lsp.with(
+        vim.lsp.diagnostic.on_publish_diagnostics,
+        {
+            virtual_text = true,
+        }
+    )
